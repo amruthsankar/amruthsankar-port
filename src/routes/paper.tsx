f0eed640c871bff0d9c29e8aee482eee9aa77d83
Amruth Sankar <amruthsankarrp@gmail.com>
2026-08-07 18:36:53 +0530

    Add PaperPage component and route

--- src/routes/paper.tsx
+++ src/routes/paper.tsx
@@ -1,1 +1,14 @@
+import { createFileRoute } from '@tanstack/react-router'
 
+export const Route = createFileRoute('/paper')({
+  component: PaperPage,
+})
+
+function PaperPage() {
+  return (
+    <div className="p-2">
+      <h3>Papers & Works</h3>
+      <p>Welcome to the Papers & Works page!</p>
+    </div>
+  )
+}

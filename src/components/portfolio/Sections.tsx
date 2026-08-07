334b009b96f3ff08e391d9201a83f59b56f1c4cd
Amruth Sankar <amruthsankarrp@gmail.com>
2026-08-07 23:56:45 +0530

    Update Sections.tsx

--- src/components/portfolio/Sections.tsx
+++ src/components/portfolio/Sections.tsx
@@ -186,9 +186,9 @@ export function Leadership() {
 
 export function Skills() {
   const technical = [
-    { name: "C Programming", level: Intermediate, icon: Cpu },
-    { name: "Python", level: Beginner, icon: Brain },
-    { name: "Microsoft Office", level: Intermediate, icon: MonitorSmartphone },
+    { name: "C Programming", level: 50, icon: Cpu },
+    { name: "Python", level: 50, icon: Brain },
+    { name: "Microsoft Office", level: 50, icon: MonitorSmartphone },
   ];
   const professional = [
     "Problem Solving",
